3abe32fff69604fb53dc30d4dccdc1591dde8711
Mariocrazy <Sibanye97@gmail.com>
2025-03-19 01:09:41 -0400

    Create code.py

---
+++ code.py
@@ -0,0 +1,7 @@
+#pn= pet name ct = Asking for the city
+
+pn = input("what is your pet's name? ")
+ct = input("what city do you live in? ")
+print(f"your twitter handle and bio @cyber{pn} from {ct}")
+
+print("Thanks for using my first program!!!")
